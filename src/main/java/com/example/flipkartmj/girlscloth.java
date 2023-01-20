f87934d22294a495e7fa0f9d8592f4c021e1052f
mahesh <mahesh.mahi.jadhav@gmail.com>
2023-01-20 17:31:00 +0530

    code1

---
+++ src/main/java/com/example/flipkartmj/girlscloth.java
@@ -0,0 +1,11 @@
+package com.example.flipkartmj;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class girlscloth {
+    @GetMapping("/girlscloth")
+    public String getData(){
+        return "only boys allowed";
+    }
+}
